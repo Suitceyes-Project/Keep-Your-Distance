333edca8095cdac062ac691e6ec8590d1f901c93
LeaBuch <lea.buchweitz@gmx.de>
2019-03-21 16:36:04 +0100

    Operations on Aruco Markers
    * created ARU_DICT class
    * created some (hopefully) useful operations on aruco markers: init of class object, show aruco markers, save aruco markers as images, get marker id from image/video cap, get marker from given id

---
+++ arucreate.py
@@ -0,0 +1,147 @@
+import cv2 as cv
+import cv2.aruco as aruco
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as py_mpl
+import imageio
+
+class ARU_DICT:
+
+# creates a custom aruco dictionary (_aruco_dict) with n aruco markers in marker size s _aruco_needed), labeled with label_1,_2,_3,_4
+# LABELS MÜSSEN NOCH DYNAMISCH WERDEN ; SIND NICHT IMMER 4
+    def __init__(self, n=4, s=4, label_1="front", label_2="left", label_3="right", label_4="back"):
+        if s == 4:
+            dict = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
+        elif s == 5:
+            dict = aruco.getPredefinedDictionary(aruco.DICT_5X5_50)
+        elif s == 6:
+            dict = aruco.getPredefinedDictionary(aruco.DICT_6X6_50)
+        elif s == 7:
+            dict = aruco.getPredefinedDictionary(aruco.DICT_7X7_50)
+        else:
+            print("no useful marker size entered! Choose marker size 4, 5, 6 or 7")
+            return
+
+        self._aruco_dict = aruco.Dictionary_create_from(n, s, dict)
+        self._aruco_needed = n
+
+        self._aruco_1 = label_1
+        self._aruco_2 = label_2
+        self._aruco_3 = label_3
+        self._aruco_4 = label_4
+
+        self.show_arucos()
+
+# ----------------------------------------------------------------------------------------------------------------------
+# creates a custom aruco dictionary (class variable _aruco_dict) with optional n = 4 aruco markers in marker size optional s = 4 (class variable _aruco_needed)
+    def create_aruco_dict(n=4, s=4):
+
+        if s == 4:
+            dict = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
+        elif s == 5:
+            dict = aruco.Dictionary_get(aruco.DICT_5X5_50)
+        elif s == 6:
+            dict = aruco.Dictionary_get(aruco.DICT_6X6_50)
+        elif s == 7:
+            dict = aruco.Dictionary_get(aruco.DICT_7X7_50)
+        else:
+            print("no useful marker size entered! Choose marker size 4, 5, 6 or 7")
+            return
+
+        _aruco_dict = aruco.Dictionary_create_from(n, s, dict)
+        _aruco_needed = n
+        #show_arucos()
+
+# ----------------------------------------------------------------------------------------------------------------------
+# shows all 4 relevant aruco markers in one window
+    def show_arucos(self):
+
+        frame = py_mpl.figure("Markers")
+        subplot1 = np.ceil(self._aruco_needed / 2)
+
+        for i in range(1, self._aruco_needed + 1):
+            win = frame.add_subplot(2, subplot1, i)
+            img = aruco.drawMarker(self._aruco_dict, i, 500)
+            py_mpl.imshow(img, cmap=mpl.cm.gray)
+            win.axis("off")
+
+        py_mpl.show()
+
+# ----------------------------------------------------------------------------------------------------------------------
+# find aruco marker i with its label and displays it
+    def lookup_aruco_nr(self, i):
+        if i == 1:
+            frame = py_mpl.figure("Aruco Nr 1 -" + self._aruco_1)
+        elif i == 2:
+            frame = py_mpl.figure("Aruco Nr 2 -" + self._aruco_2)
+        elif i == 3:
+            frame = py_mpl.figure("Aruco Nr 3 -" + self._aruco_3)
+        elif i == 4:
+            frame = py_mpl.figure("Aruco Nr 4 -" + self._aruco_4)
+        else:
+            print("This marker is unknown for this application. Known marker ids: \n id_1: front \n id_2: left \n id_3: right \n id_4: back")
+            return
+
+        img = aruco.drawMarker(self._aruco_dict, i - 1, 500)
+        py_mpl.imshow(img, cmap=mpl.cm.gray)
+        py_mpl.axis("off")
+        py_mpl.show()
+
+# ----------------------------------------------------------------------------------------------------------------------
+# detects markers from respective custom dictionary in an image or captured video and prints out the information
+    def lookup_aruco_img(self):
+        # get img for comparison
+        #checkimg = cv.imread("_markers/marker_advanced.jpg")
+
+        # get camera image for comparison
+        cap = cv.VideoCapture(0)
+
+        while(True):
+            # Capture frame-by-frame
+            ret, frame = cap.read()
+
+            # Our operations on the frame come here
+            # GREY DOES NOT SEEM TO BE A GOOD IDEA
+            #gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+            parameters = aruco.DetectorParameters_create()
+            corners, ids, rejectedImgPoints = aruco.detectMarkers(frame, self._aruco_dict, parameters=parameters)
+
+            frame_markers = aruco.drawDetectedMarkers(frame, corners, ids)
+
+            if ids is not None:
+                py_mpl.figure()
+                py_mpl.imshow(frame_markers, origin = "upper")
+                for i in range(0, len(ids)):
+                    c = corners[i][0]
+                    py_mpl.plot([c[:, 0].mean()], [c[:, 1].mean()], "+", label = "Marker = {0}".format(ids[i]+1))
+                py_mpl.legend()
+                py_mpl.axis("off")
+
+                # print/show image with information about recognized markers
+                #py_mpl.show()
+                i_str = str(ids[i]+1)
+                print("recognized marker: " + i_str)
+
+            # Display the resulting frame
+            cv.imshow('video frame', frame)
+            if cv.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        # When everything done, release the capture
+        cap.release()
+        cv.destroyAllWindows()
+
+# ----------------------------------------------------------------------------------------------------------------------
+# saves all needed (_aruco_needed) markers from custom dictionary (_aruco_dict) to the folder _markers
+    def save_aruco_images(self):
+
+        if isinstance(self._aruco_needed, int):
+            for i in range(0, self._aruco_needed):
+                img = aruco.drawMarker(self._aruco_dict, i, 500)
+                i_str = str(i+1)
+                imageio.imwrite("_markers/marker_"+i_str+".jpg", img)
+                print("Marker", i_str, "ist gespeichert.")
+
+# ----------------------------------------------------------------------------------------------------------------------
+
--- aruloc.py
+++ aruloc.py
@@ -1,21 +1,26 @@
-import cv2;
-import cv2.aruco;
-import numpy as np;
+import cv2 as cv
+import arucreate
 
-cap = cv2.VideoCapture(0)
+# captures camera view
+def camera_cap():
+    cap = cv.VideoCapture(0)
 
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+    while(True):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
 
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # Our operations on the frame come here
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
-    # Display the resulting frame
-    cv2.imshow('frame',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        # Display the resulting frame
+        cv.imshow('video frame', gray)
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            break
 
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+    # When everything done, release the capture
+    cap.release()
+    cv.destroyAllWindows()
+
+
+police_chase_dic = arucreate.ARU_DICT(4, 4,"front", "left", "right", "back")
+#camera_cap()
